41f1e3e437e99359ce343f9662fe881d5dbd7e37
Alexander Altman <qewhouse@gmail.com>
2024-06-09 20:29:21 +0300

    add gitignore

---
+++ EpicRPS/App/SceneDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  SceneDelegate.swift
+//  EpicRPS
+//
+//  Created by Alexander Altman on 09.06.2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = MainViewController()
+        window?.makeKeyAndVisible()
+    }
+   }
