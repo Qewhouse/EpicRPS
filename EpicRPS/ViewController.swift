4e9220e8516abb9d4d715969388edc06f8af1651
Alexander Altman <qewhouse@gmail.com>
2024-06-09 19:40:36 +0300

    Initial Commit

---
+++ EpicRPS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EpicRPS
+//
+//  Created by Alexander Altman on 09.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
