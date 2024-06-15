b17506cc788c240f776209498bced77381d0f97d
STASART <121148380+STASART@users.noreply.github.com>
2024-06-15 14:15:44 +0300

    FixTimerAndResetGame
    FixTimer, CreateFisrstStartService and Configure start Defaults, Added tatal score in Defaults, FixResetGame of ResultVc,

---
+++ EpicRPS/FirstStartCheck/FirstStartChekManager.swift
@@ -0,0 +1,23 @@
+//
+//  FirstStartChekManager.swift
+//  EpicRPS
+//
+//  Created by Станислав Артамонов on 15.06.24.
+//
+
+import Foundation
+
+final class FirstStartChekService: NSObject {
+    func startChekAndConfigureDefaults() {
+        if DefaultsSettings.isFirstStart == nil {
+            DefaultsSettings.femaleWinPlayerScore = 0
+            DefaultsSettings.femaleLoosePlayerScore = 0
+            DefaultsSettings.maleWinPlayerScore = 0
+            DefaultsSettings.maleLoosePlayerScore = 0
+            DefaultsSettings.maxRoundCount = 3
+            DefaultsSettings.pvpMode = false
+            DefaultsSettings.roundTime = 30
+            DefaultsSettings.isFirstStart = true
+        }
+    }
+}
